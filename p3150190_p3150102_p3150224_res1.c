d65395fd24274f15e36791e885519aad20f69102
giannisfourfouris <giannisfourfouris@gmail.com>
2019-04-07 17:37:39 +0300

    eimaste se kalo dromo

--- p3150190_p3150102_p3150224_res1.c
+++ p3150190_p3150102_p3150224_res1.c
@@ -1,126 +1,203 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <time.h>
 #include"p3150190_p3150102_p3150224_res1.h"
 #include <pthread.h>
 
-#include <unistd.h>
+int Ncust;
+int seed;
+int account=0;
+int counterTransaction=0;
+int *seatArray;
+//krataei an apetyxe h oxi ena thread
+int rc;
+int *threadId;
+int *standByTime;
+int *handlingTime;
+struct timespec start, stop;
+pthread_mutex_t lock;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-int logariasmos=0;
-int arithmosSunalagwn=0;
-int pelates=100;
-pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-/*
- * O counter einai ekswteriki metavliti gia na ton vlepoun ola ta threads.
-*/
-int counter;
-
-
-pthread_mutex_t mutexcount;
 
 /**
- * H synartisi pou tha kaleitai apo ta threads kai tha einai ypeythini gia tin ayksisi tou counter.
+ * H synartisi pou tha kaleite apo ta threads gia na typwnei to mynima 
+ * Thread: Hello World from thread threadId
 */
-void *increaseCounter(void *threadId) {
-	int *tid = (int *)threadId;
-    	int prevValue, rc;
-	//kleidwma tou mutex protou allaksoume tin timi
-	rc = pthread_mutex_lock(&mutexcount);
-	if (rc != 0) {	
-		printf("ERROR: return code from pthread_mutex_lock() is %d\n", rc);
-		pthread_exit(&rc);
-	}
+ void *customer(void *x){
+ 	int id = (int *)x;
+	
+ 	printf("Hello from pelati: %d\n",id);
+ 	rc = pthread_mutex_lock(&lock);
+	if (rc != 0) {
+			printf("ERROR: return code from pthread_mutex_lock is %d\n", rc);
+			exit(-1);		
+		}
 
-	printf("Thread %d: About to increase the counter.\n", *tid);
-	prevValue = counter;
-	counter++;
-	sleep(1);
-	printf("Thread %d: Previous value = %d, New value = %d.\n", *tid, prevValue, counter);
+ 	while (Ntel == 0) {
+ 		printf("O pelatis %d, den brike diathesimo tilefoniti.Blocked...\n", id);
+ 		rc = pthread_cond_wait(&cond, &lock);
+		if (rc != 0) {
+			printf("ERROR: return code from pthread_cond_wait is %d\n", rc);
+			exit(-1);		
+		}
+ 	}
+ 	printf("O pelatis %d, eksipiretite.\n",id);
+ 	int randomTicketNumber=rand_r(seed)%Nseathigh +Nseatlow
+ 	Ntel--;
+ 	rc = pthread_mutex_unlock(&lock);
+	if (rc != 0) {
+			printf("ERROR: return code from pthread_mutex_unlock is %d\n", rc);
+			exit(-1);		
+		}
 
- 
+ 	sleep(5); //kane kapoia douleia me ton tilefoniti
 
-	rc = pthread_mutex_unlock(&mutexcount);
-	if (rc != 0) {	
-		printf("ERROR: return code from pthread_mutex_unlock() is %d\n", rc);
-		pthread_exit(&rc);
-	}
 
-	pthread_exit(threadId);
-}
+ 	rc = pthread_mutex_lock(&lock);
+	if (rc != 0) {
+			printf("ERROR: return code from pthread_mutex_lock is %d\n", rc);
+			exit(-1);		
+		}
+ 	printf("O pelatis %d eksipiretithike epitixos! \n", id);
+ 	Ntel++;
+ 	rc = pthread_cond_signal(&cond);
+	if (rc != 0) {
+			printf("ERROR: return code from pthread_cond_signal is %d\n", rc);
+			exit(-1);		
+		}
+	 rc = pthread_mutex_unlock(&lock);
+	if (rc != 0) {
+			printf("ERROR: return code from pthread_mutex_unlock is %d\n", rc);
+			exit(-1);		
+		}
+	/*aparaitito gia na gnwrizei o pateras oti to thread termatise swsta,
+	douleuei swsta kai an kanete return kapoia timi.*/
+ 	pthread_exit(id); //return
+ }
 
-int main(int argc, char** argv) {  
-        
-printf("Nseat: %d \n",Nseat);
-printf("Ntel: %d \n",Ntel);
-printf("Nseatlow: %d \n",Nseatlow);
-printf("Nseathigh: %d \n",Nseathigh);
-printf("tseatlow: %d \n",tseatlow);
-printf("tseathigh: %d \n",tseathigh);
-printf("Nseat: %lf \n",Pcardsucces);
-printf("Cseat: %d \n",Cseat);
-printf("logariasmos: %d \n",logariasmos);
-printf("arithmosSunalagwn: %d \n",arithmosSunalagwn);
 
-int a=time(NULL);
-int num=rand_r(&a)%Nseathigh+Nseatlow;
-printf("num: %d \n",num);
 
-int rc;
+
+int main(int argc, char** argv) {
+	//dhmioyrgia pinaka pthread      
 	pthread_t *threads;
 
-	threads = malloc(pelates * sizeof(pthread_t));
+
+    //treia arguments (to prwto orisma einai to onoma tou programmatos to deftero einai
+    // to Ncust kai to trito einai to seed).
+	if (argc != 3) {
+		printf("ERROR: Provide two arguments.\n");
+		return -1;
+	}
+      
+     Ncust=atoi(argv[1]);
+     seed=atoi(argv[2]); 
+     printf("first argument: %d, seed einai: %d\n", Ncust,seed);
+
+	//xrisi malloc gia dimiourgia tou pinaka me megethos 250.
+	//desmeuoume sizeof(int) * 250 sti mnimi.
+	seatArray = (int *) malloc(sizeof(int) * 250);
+
+	//elegxos an apetyxe i malloc
+	if (seatArray == NULL) {
+		printf("ERROR: Malloc failed not enough memory!\n");
+		return -1;
+	}
+
+   	//arxikopoihsh pinaka 250 thesewn
+	for (int i = 0; i < 250; i++) {
+		seatArray[i] =02;
+	}
+       
+	//desmeysh mnhmhs gia ta threads
+        threads = malloc( Ncust* sizeof(pthread_t));
 	if (threads == NULL) {
 		printf("NOT ENOUGH MEMORY!\n");
 		return -1;
 	}
-	//arxikopoiisi counter
-	counter = 0;
 
-	//arxikopoiisi tou mutex.
-	rc = pthread_mutex_init(&mutexcount, NULL);
-	if (rc != 0) {	
-		printf("ERROR: return code from pthread_mutex_init() is %d\n", rc);
-		exit(-1);
+      
+   	
+       //xrisi malloc gia dimiourgia tou pinaka me megethos Ncust.
+	//desmeuoume sizeof(int) * Ncust sti mnimi.
+	threadId = (int *) malloc(sizeof(int) * Ncust);
+
+	//elegxos an apetyxe i malloc
+	if (threadId == NULL) {
+		printf("ERROR: Malloc failed not enough memory!\n");
+		return -1;
 	}
- int threadIds[pelates];
-void *status;
-	int i = 0;
-	for (i = 0; i < pelates; i++) {
-		threadIds[i] = i + 1;
-		printf("Main: creating thread %d\n", threadIds[i]);
-		
-		/*dimiourgia tou thread*/
-    		rc = pthread_create(&threads[i], NULL, increaseCounter, &threadIds[i]);
-                //sleep(2);
+
+	//xrisi malloc gia dimiourgia tou pinaka me megethos Ncust.
+	//desmeuoume sizeof(int) * Ncust sti mnimi.
+	standByTime = (int *) malloc(sizeof(int) * Ncust);
+
+	//elegxos an apetyxe i malloc
+	if (standByTime == NULL) {
+		printf("ERROR: Malloc failed not enough memory!\n");
+		return -1;
+	}
+
+	//xrisi malloc gia dimiourgia tou pinaka me megethos Ncust.
+	//desmeuoume sizeof(int) * Ncust sti mnimi.
+	handlingTime = (int *) malloc(sizeof(int) * Ncust);
+
+	//elegxos an apetyxe i malloc
+	if (handlingTime == NULL) {
+		printf("ERROR: Malloc failed not enough memory!\n");
+		return -1;
+	}
+
+        //arxikopoihsh lock
+        pthread_mutex_init(&lock, NULL);
+
+	//create threads
+   	for(int threadCount = 0; threadCount < Ncust; threadCount++) {
+    		//printf("Main: creating thread %d\n", threadCount);
+		//arxikopoihsh threadId
+		threadId[threadCount] = threadCount + 1;
+		printf("Main: creating thread %d with id: %d\n", threadCount,threadId[threadCount]);
+		/*dimiourgia tou thread, n allakse prin hello!!!!*/
+    	        rc=pthread_create(&threads[threadCount],NULL,customer,threadId[threadCount]); 
+		//dhmioyrgia xronou anamonis/pelati kai xronoy kai xronou eksipiretisis/pelatis
+		handlingTime[threadCount]=clock_gettime( CLOCK_REALTIME, &start);
+		standByTime[threadCount]=clock_gettime( CLOCK_REALTIME, &start);
+
 		/*elegxos oti to thread dimiourgithike swsta.*/
     		if (rc != 0) {
     			printf("ERROR: return code from pthread_create() is %d\n", rc);
        			exit(-1);
        		}
+    	}
 
-		rc = pthread_join(threads[i], &status);
-            
+       /*Aparaitito gia na stamatisei to thread, an den to orisete yparxei
+	periptwsi na teleiwsei o pateras prin ta threads kai ara na min exoume
+	to epithymito apotelesma*/
+	for (int threadCount = 0; threadCount < Ncust; threadCount++) {
+		rc = pthread_join(threads[threadCount], NULL);
 		
 		if (rc != 0) {
 			printf("ERROR: return code from pthread_join() is %d\n", rc);
 			exit(-1);		
 		}
 
-		printf("Main: Thread %d finished with status %d.\n", threadIds[i], *(int *)status);
+		printf("Main: Thread %d returned.\n", threadId[threadCount]);
 	}
- 
-	
-	
 
-	printf("Counter value after threads finished = %d.\n", counter);
 
-	//"katastrofi" tou mutex.
-	rc = pthread_mutex_destroy(&mutexcount);
-	if (rc != 0) {
-   		printf("ERROR: return code from pthread_mutex_destroy() is %d\n", rc);
-      		exit(-1);
-	}
-free(threads);
-	return 1;
+
+    //panta eletherwnoume ti mnimi pou exoume desmeusei.
+    free(seatArray);
+    free(threads);
+    free(threadId);
+    free(handlingTime);
+    free(standByTime); 
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
+
+return 0;
+
+
 }
+
+
--- p3150190_p3150102_p3150224_res1.h
+++ p3150190_p3150102_p3150224_res1.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int Nseat=250; 
 int Ntel=8; 
 int Nseatlow=1; 
@@ -6,5 +7,5 @@ int Nseathigh=5;
 int tseatlow=5;
 int tseathigh=10; 
 double Pcardsucces=0.9;
-int Cseat=20 ;
+int Cseat=20;
 
